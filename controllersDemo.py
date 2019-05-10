77a88852336aacc00e8e8ff03b64ea90fe36cce8
Nathaniel Chien <nchien2@stanford.edu>
2019-05-10 16:13:03 -0700

    Made code prettier

--- controllersDemo.py
+++ controllersDemo.py
@@ -26,27 +26,44 @@ class Form(FlaskForm):
 def main():
     if(request.method == 'POST'):
         input = request.form
+        # document = types.Document(
+        #     content=input['input'],
+        #     type=enums.Document.Type.PLAIN_TEXT)
+        #
+        # print('=============SYNTAX=============')
+        # getSyntax(document)
+        # print('\n=============SENTIMENT=============')
+        # getSentiment(document)
+        # print('\n=============ENTITIES=============')
+        # getEntities(document)
+        # print('\n=============CATEGORIES=============')
+        # getCategories(document)    
     return render_template(
         'template.html',
         form = Form()
     )
 
-def getSyntax(input):
+def getSyntax(document):
     #Detects parts of speech, punctuation, etc.
     pass
 
-def getSentiment(input):
+def getSentiment(document):
     #Uses google's pre-trained NLP algorithm to determine 'feeling' of text
     #Has a 'score' that corresponds to overall emotional leaning and 'magnitude'
     #that includes overall strength of emotion, both positive and negative.
     pass
 
-def getEntities(input):
+def getEntities(document):
     #Classifies phrases in the text into entities, with different categories like
     #person, organization, location, etc. Also gives a measure of salience, or
     #the entities' importance for the text.
+    # Instantiates a client
     pass
 
+def getCategories(document):
+    #Guesses what the text is about, eg "Technology", "Business".  Requires
+    #at least 20 words
+    pass
 
 if __name__ == "__main__":
     app.run()
